328d432ff9ef61c604ef8d3090b2a0363ecadf42
Andres Felipe <andrortiz97@hotmail.com>
2024-03-03 11:54:04 -0500

    Deleted unnecesary lines

--- Produndizacion/Clase/Models/Attendee.cs
+++ Produndizacion/Clase/Models/Attendee.cs
@@ -8,8 +8,6 @@ namespace Clase.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int IdAttendee { get; set; }
-
-
         public string First_Name { get; set; }
         public string Last_Name { get; set; }
         public string Email { get; set; }
